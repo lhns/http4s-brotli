d4f0cebadeb2bd285bdb2132e47ee814572f0be2
Mat Mannion <mathew.mannion@linklaters.com>
2023-11-06 11:29:59 +0000

    Add http4s client middleware, fixes #7

--- build.sbt
+++ build.sbt
@@ -26,7 +26,8 @@ developers := List(
 
 libraryDependencies ++= Seq(
   "de.lhns" %% "fs2-compress-brotli" % "0.5.0",
-  "org.http4s" %% "http4s-core" % "0.23.19",
+  "org.http4s" %% "http4s-core"   % "0.23.19",
+  "org.http4s" %% "http4s-client" % "0.23.19",
 )
 
 addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
--- src/main/scala/de/lolhens/http4s/brotli/BrotliMiddleware.scala
+++ src/main/scala/de/lolhens/http4s/brotli/BrotliMiddleware.scala
@@ -1,9 +1,13 @@
 package de.lolhens.http4s.brotli
 
+import cats.ApplicativeThrow
 import cats.effect.kernel.Async
 import de.lhns.fs2.compress.BrotliDecompressor
+import fs2.{Pipe, Pull, Stream}
 import org.http4s.headers.{`Content-Encoding`, `Content-Length`}
 import org.http4s.{ContentCoding, HttpRoutes, Message, Request}
+
+import scala.util.control.NoStackTrace
 
 object BrotliMiddleware {
   def decompress[F[_] : Async](message: Message[F],
@@ -11,12 +15,28 @@ object BrotliMiddleware {
     message.headers.get[`Content-Encoding`].map(_.contentCoding) match {
       case Some(ContentCoding.br) =>
         message
-          .withBodyStream(message.body.through(BrotliDecompressor[F](chunkSize).decompress))
+          .withBodyStream(message.body.through(decompressWith(BrotliDecompressor[F](chunkSize).decompress)))
           .removeHeader[`Content-Encoding`]
           .removeHeader[`Content-Length`]
 
       case _ => message.covary
     }
+
+  private def decompressWith[F[_] : ApplicativeThrow](decompressor: Pipe[F, Byte, Byte]): Pipe[F, Byte, Byte] =
+    _.pull.peek1
+      .flatMap {
+        case None => Pull.raiseError(EmptyBodyException)
+        case Some((_, fullStream)) => Pull.output1(fullStream)
+      }
+      .stream
+      .flatten
+      .through(decompressor)
+      .handleErrorWith {
+        case EmptyBodyException => Stream.empty
+        case error => Stream.raiseError(error)
+      }
+
+  private object EmptyBodyException extends Throwable with NoStackTrace
 
   def apply[F[_] : Async](routes: HttpRoutes[F],
                           chunkSize: Int = BrotliDecompressor.defaultChunkSize): HttpRoutes[F] =
