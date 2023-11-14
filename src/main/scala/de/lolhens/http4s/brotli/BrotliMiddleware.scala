d3f563fd66003896c3830fee185b458f7d31582b
Pierre Kisters <1524059+lhns@users.noreply.github.com>
2023-11-14 10:30:57 +0100

    Update BrotliMiddleware.scala

--- src/main/scala/de/lolhens/http4s/brotli/BrotliMiddleware.scala
+++ src/main/scala/de/lolhens/http4s/brotli/BrotliMiddleware.scala
@@ -6,19 +6,19 @@ import org.http4s.headers.{`Content-Encoding`, `Content-Length`}
 import org.http4s.{ContentCoding, HttpRoutes, Message, Request}
 
 object BrotliMiddleware {
-  def decompress[F[_] : Async](message: Message[F],
-                               chunkSize: Int = BrotliDecompressor.defaultChunkSize): message.Self =
+  def decompress[F[_] : Async: BrotliDecompressor](message: Message[F],
+                                                   chunkSize: Int = BrotliDecompressor.defaultChunkSize): message.Self =
     message.headers.get[`Content-Encoding`].map(_.contentCoding) match {
       case Some(ContentCoding.br) =>
         message
-          .withBodyStream(message.body.through(BrotliDecompressor[F](chunkSize).decompress))
+          .withBodyStream(message.body.through(BrotliDecompressor[F].decompress))
           .removeHeader[`Content-Encoding`]
           .removeHeader[`Content-Length`]
 
       case _ => message.covary
     }
 
-  def apply[F[_] : Async](routes: HttpRoutes[F],
-                          chunkSize: Int = BrotliDecompressor.defaultChunkSize): HttpRoutes[F] =
+  def apply[F[_] : Async: BrotliDecompressor](routes: HttpRoutes[F],
+                                              chunkSize: Int = BrotliDecompressor.defaultChunkSize): HttpRoutes[F] =
     routes.local[Request[F]](decompress(_, chunkSize))
 }
